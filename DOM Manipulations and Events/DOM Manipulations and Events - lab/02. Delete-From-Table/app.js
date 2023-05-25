b7fd93b373933284d512f4f0f68438fe4a126a96
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-25 17:30:10 +0300

    UP

--- DOM Manipulations and Events/DOM Manipulations and Events - lab/02. Delete-From-Table/app.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - lab/02. Delete-From-Table/app.js
@@ -2,16 +2,10 @@ function deleteByEmail() {
  let inputEmail = document.querySelector('input[name="email"]');
  const emailsNames = document.querySelectorAll('tr td:nth-of-type(even)');
  const result = document.getElementById('result');
-
  const arrEmails= Array.from(emailsNames);
  const searchEmails = arrEmails.find(mail=> mail.textContent === inputEmail.value);
-
- if(searchEmails){
-    searchEmails.parentElement.remove();
-    result.textContent = 'Delete.'
-
- }else{
-    result.textContent= 'Not found.'
- }
+ 
+ if(searchEmails) searchEmails.parentElement.remove();
+ result.textContent=searchEmails ? "Delete.":"Not found."
  inputEmail.value=''
 }

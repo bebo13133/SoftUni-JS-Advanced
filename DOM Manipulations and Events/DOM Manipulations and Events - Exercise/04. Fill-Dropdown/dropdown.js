bb7f393ff01fd1fd5d7f889e880f3b00b67ab55c
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-27 20:50:25 +0300

    Update dropdown.js

--- DOM Manipulations and Events/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const text = document.getElementById('newItemText');
+    const inputValue = document.getElementById('newItemValue');
+    const optionField = document.createElement('option')
+    optionField.text = text.value;
+    optionField.value = inputValue.value;
+    document.getElementById('menu')
+    .appendChild(optionField)
+    text.value = '';
+    inputValue.value = '';
 }

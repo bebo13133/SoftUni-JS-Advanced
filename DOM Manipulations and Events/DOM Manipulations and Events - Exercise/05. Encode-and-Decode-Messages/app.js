28c64b7cdb5537db71946708e7e75cb4ea5d8666
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-27 21:49:47 +0300

    Update app.js

--- DOM Manipulations and Events/DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,11 +1,9 @@
 function encodeAndDecodeMessages() {
     const textAreas = document.querySelectorAll('textarea');
-    const buttons = [...document.querySelectorAll('button')]
-    .forEach(button => button.addEventListener('click', onClick));
+    const buttons = [...document.querySelectorAll('button')].forEach(button => button.addEventListener('click', onClick));
     function onClick(event) {
         const inputText = event.target.parentElement.querySelector('textarea');
         if (inputText.value != '') {
-
             if (event.target.textContent === 'Encode and send it') {
                 console.log("decodeText")
                 const decodeText = (inputText.value).split('').reduce((acc, char) => {

5397be1d36525f352e0e16948b9cd26d36644343
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-27 19:50:35 +0300

    upload

--- DOM Manipulations and Events/DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,13 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button')).forEach((buttons) => buttons.addEventListener('click',
+        (event) => {
+            const profile = event.target.parentElement;
+            const isActive = profile.querySelector('input[value="unlock"]').checked;
+            if (isActive) {
+                const infoField = Array.from(profile.querySelectorAll('div')).find(element => element.id.includes('HiddenFields'));
+                (event.target.textContent === "Show more") ?
+                (event.target.textContent = "Hide it", infoField.style.display = "block") 
+                :(event.target.textContent = "Show more", infoField.style.display = "none");
+            };
+        }));
 }

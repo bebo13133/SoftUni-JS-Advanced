65ee558a74009f7454f7c48e395467f759e41a14
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-27 13:01:56 +0300

    add

--- DOM Manipulations and Events/DOM Manipulations and Events - lab/07. Shopping-Cart/solution.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - lab/07. Shopping-Cart/solution.js
@@ -1,28 +1,20 @@
 function solve() {
-//capter element 
 document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
 document.getElementsByClassName('checkout')[0].addEventListener('click', onCheckOut);
-
 
 const cart= [];
 const output = document.getElementsByTagName('textarea')[0];
 output.value = "";
 
 function onClick(event){
-
    if (event.target.tagName === "BUTTON" && event.target.classList.contains('add-product')){
    const product = event.target.parentElement.parentElement;
-   
    const nameProduct = product.querySelector('.product-title').textContent;
    const price = Number(product.querySelector('.product-line-price').textContent);
-   cart.push({
-      nameProduct, 
-      price,
-   });
+   cart.push({nameProduct,price,});
    output.value+= `Added ${nameProduct} for ${price.toFixed(2)} to the cart.\n`;
 }
 }
-
 function onCheckOut(){
    const allProducts = new Set()
    cart.forEach(product=> allProducts.add(product.nameProduct));

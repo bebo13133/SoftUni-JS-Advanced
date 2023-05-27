043d3fd8e38dd2ae54d000d142645c47729a152f
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-27 18:13:01 +0300

    add

--- DOM Manipulations and Events/DOM Manipulations and Events - Exercise/01. Sections/script.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+ const parent = document.getElementById('content')
+ words.forEach(element => {
+  const div  = document.createElement('div');
+  const par = document.createElement('p');  
+par.textContent = element
+par.style.display = 'none' 
+div.appendChild(par);
+
+div.addEventListener('click',(event)=>{
+   event.target.children[0].style.display ="block"
+})
+parent.appendChild(div)
+});
 }
--- DOM Manipulations and Events/DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,45 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let timeObj = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400,
+    }
+    const daysInput = document.getElementById('days');
+    const minutesInput = document.getElementById('minutes');
+    const hoursInput = document.getElementById('hours');
+    const secondsInput = document.getElementById('seconds')
+
+
+
+    Array.from(document.querySelectorAll('input[type="button"]')).forEach((x) => x
+        .addEventListener('click', (event) => {
+            const input = event.target.parentElement.querySelector('input[type="text"]')
+            const inputValue = Number(input.value);
+            const idInput = input.id
+
+            const timeConvert = convert(inputValue, idInput)
+
+            daysInput.value = timeConvert.days;
+            hoursInput.value = timeConvert.hours;
+            minutesInput.value = timeConvert.minutes;
+            secondsInput.value = timeConvert.seconds;
+
+        }));
+
+
+    function convert(value, id) {
+        const day = value / timeObj[id]
+
+        return {
+            days: day,
+            hours: day * timeObj.hours,
+            minutes: day * timeObj.minutes,
+            seconds: day * timeObj.seconds,
+        };
+
+
+    }
+
 }

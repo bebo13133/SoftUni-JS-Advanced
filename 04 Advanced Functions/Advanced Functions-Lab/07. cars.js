3d60d39c3ac1e3f5f67d543606c6dbbc774a4be1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-30 13:23:48 +0300

    update

--- 04 Advanced Functions/Advanced Functions-Lab/07. cars.js
+++ 04 Advanced Functions/Advanced Functions-Lab/07. cars.js
@@ -1,21 +1,18 @@
 function cars (input){
- const data={} 
-
-const returnResult = {
-    create:(name, inherit, parentName)=>{data[name]= inherit ? Object.create(data[parentName]):{}},
-    set: (name, key, value)=>{data[name][key]=value},
-
-    print: (name)=>{
-         const entry = [];
-        for(let key in data[name]){
-            entry.push(`${key}:${data[name][key]}`);
-        }
-        console.log(entry.join(','));
-    },
-}
-const lines  = input.forEach(line => {const [command, name, key, value] = line.split(" ");
-returnResult[command](name,key,value)});
-}
+    const data={} 
+   const returnResult = {
+       create:(name, inherit, parentName)=>{data[name]= inherit ? Object.create(data[parentName]):{}},
+       set: (name, key, value)=>{data[name][key]=value},
+   
+       print: (name)=>{
+            const entry = [];
+           for(let key in data[name]) entry.push(`${key}:${data[name][key]}`);
+           console.log(entry.join(','));
+       },
+   }
+   const lines  = input.forEach(line => {const [command, name, key, value] = line.split(" ");
+   returnResult[command](name,key,value)});
+   }
 
 
 

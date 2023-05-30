324e1a92291655982258d0d7d670733186468cc6
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-30 11:59:21 +0300

    push

--- 04 Advanced Functions/Advanced Functions-Lab/07. cars.js
+++ 04 Advanced Functions/Advanced Functions-Lab/07. cars.js
@@ -1,12 +1,31 @@
+function cars (input){
+ const data={} 
+
+const returnResult = {
+    create:(name, inherit, parentName)=>{data[name]= inherit ? Object.create(data[parentName]):{}},
+    set: (name, key, value)=>{data[name][key]=value},
+
+    print: (name)=>{
+         const entry = [];
+        for(let key in data[name]){
+            entry.push(`${key}:${data[name][key]}`);
+        }
+        console.log(entry.join(','));
+    },
+}
+const lines  = input.forEach(line => {const [command, name, key, value] = line.split(" ");
+returnResult[command](name,key,value)});
+}
 
 
 
 
 
-['create c1',
+
+cars(['create c1',
 'create c2 inherit c1',
 'set c1 color red',
 'set c2 model new',
 'print c1',
-'print c2']
+'print c2'])
 

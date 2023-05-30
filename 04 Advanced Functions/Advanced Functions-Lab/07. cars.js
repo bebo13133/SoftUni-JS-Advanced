48bad823cd2fb33206477a69eb6c7f736c51e5cd
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-30 13:27:03 +0300

    Update 07. cars.js

--- 04 Advanced Functions/Advanced Functions-Lab/07. cars.js
+++ 04 Advanced Functions/Advanced Functions-Lab/07. cars.js
@@ -1,18 +1,19 @@
-function cars (input){
-    const data={} 
-   const returnResult = {
-       create:(name, inherit, parentName)=>{data[name]= inherit ? Object.create(data[parentName]):{}},
-       set: (name, key, value)=>{data[name][key]=value},
-   
-       print: (name)=>{
+function cars(input) {
+    const data = {}
+    const returnResult = {
+        create: (name, inherit, parentName) => { data[name] = inherit ? Object.create(data[parentName]) : {} },
+        set: (name, key, value) => { data[name][key] = value },
+        print: (name) => {
             const entry = [];
-           for(let key in data[name]) entry.push(`${key}:${data[name][key]}`);
-           console.log(entry.join(','));
-       },
-   }
-   const lines  = input.forEach(line => {const [command, name, key, value] = line.split(" ");
-   returnResult[command](name,key,value)});
-   }
+            for (let key in data[name]) entry.push(`${key}:${data[name][key]}`);
+            console.log(entry.join(','));
+        },
+    }
+    const lines = input.forEach(line => {
+        const [command, name, key, value] = line.split(" ");
+        returnResult[command](name, key, value)
+    });
+}
 
 
 
@@ -20,9 +21,9 @@ function cars (input){
 
 
 cars(['create c1',
-'create c2 inherit c1',
-'set c1 color red',
-'set c2 model new',
-'print c1',
-'print c2'])
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2'])
 

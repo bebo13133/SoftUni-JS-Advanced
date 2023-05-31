057ca418c9c0656bb1020e7e5701f2ebaed8c7a6
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-31 14:10:59 +0300

    Create 02.playingCards.js

---
+++ 05 Unit-Testingand-Error-Handling/01 Unit Testing and Error Handling - Lab/02.playingCards.js
@@ -0,0 +1,19 @@
+function playingCard(face, suits) {
+    const faceCard = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"]
+    const suitsCard = { S: "\u2660", H: "\u2665", D: "\u2666", C: "\u2663" }
+
+    if (!faceCard.includes(face)) throw new Error('Invalid face ' + face)
+    //може още една проверка да има !! 
+    //  if(suits[suit] === undefined){
+    //     throw new Error('Invalid suit ' + suit);
+    // }
+
+    const cards = {
+        face,
+        suits,
+        toString() {
+            return face + suitsCard[suits]
+        }
+    };
+    return cards
+}

2ee8e0f606c15ab1ed89dcd65ba788fa445f2130
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-01 16:21:06 +0300

    up

--- 05 Unit-Testingand-Error-Handling/02 Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ 05 Unit-Testingand-Error-Handling/02 Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,7 +1,6 @@
 function validate() {
     const validEmail = document.getElementById('email')
-    validEmail.addEventListener('change', (e)=>{
-     
+    validEmail.addEventListener('change', (e)=>{     
         const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/g
         const operation = pattern.test(e.target.value)? "remove":"add"
         e.target.classList[operation]("error")
--- 05 Unit-Testingand-Error-Handling/02 Unit Testing and Error Handling - Exercise/07.Form-Validation/formValidation.js
+++ 05 Unit-Testingand-Error-Handling/02 Unit Testing and Error Handling - Exercise/07.Form-Validation/formValidation.js
@@ -1,3 +1,35 @@
 function validate() {
-    // TODO
+    document.getElementById('company').addEventListener('change', (e) => {
+        let companyInfo = document.getElementById('companyInfo');
+        document.getElementById('company').cheked == false ? companyInfo.style.display = 'none' : companyInfo.style.display = 'block'
+    });
+    document.getElementById('submit').addEventListener('click', onClick);
+
+    function onClick(event) {
+        event.preventDefault();
+        const invalidFields = [];
+        const checkBox = document.querySelector('#company');
+        const userName = document.getElementById('username');
+
+        if (!/^[A-Za-z0-9]{3,20}$/.test(userName.value))  invalidFields.push(userName);
+        // /^[^@.]+@[^@]*\.[^@]+$/;
+        const mailField = document.getElementById('email');
+      
+        if (!/^[^@.]+@[^@]*\.[^@]*$/.test(mailField.value)) invalidFields.push(mailField);
+        const pass = document.getElementById('password');
+        const confirmPass = document.getElementById('confirm-password');
+      
+        if (!/^[\w]{5,15}$/.test(pass.value) || confirmPass.value !== pass.value || confirmPass.value === '') {
+            invalidFields.push(pass);
+            invalidFields.push(confirmPass);
+        }
+        if (checkBox.checked) {
+            const copmanyId = document.getElementById('companyNumber');
+            if (!/^[0-9]{4}$/.test(copmanyId.value)) invalidFields.push(copmanyId);
+            
+        };
+  
+        invalidFields.forEach(field => field.style.borderColor = 'red');
+        !invalidFields.length ? document.querySelector('#valid').style.display = 'block' : document.querySelector('#valid').style.display = 'none';
+    }
 }

748f271d4ebc6028a39ec2136f43c3178367dfa7
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-04 14:27:40 +0300

    Update script.js

--- 04 Advanced Functions/Advanced Functions - Exercise/08. Next Article/script.js
+++ 04 Advanced Functions/Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,14 +1,12 @@
 function getArticleGenerator(articles) {
     const output = document.getElementById("content")
     const copyArt = articles.slice()
-
     return function showNext() {
-        if (copyArt.length !== 0) {
+        if (copyArt[0] !== undefined) { // copyArt.length != 0 -също върши работа 
             const article = document.createElement('article')
             article.textContent = copyArt.shift()
-            output.appendChild(article)
+            output.appendChild(article);
         }
         // return showNext;
     }
-
 }

088252ecc54a4afa8a7de584d5dcce57071bee1d
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-04 09:44:48 +0300

    modify

--- 04 Advanced Functions/Advanced Functions - Exercise/02argumentInfo.js
+++ 04 Advanced Functions/Advanced Functions - Exercise/02argumentInfo.js
@@ -1,12 +1,12 @@
 function argumentInfo(){
     let obj= {}
-let result = [...arguments].forEach(element=>{
+let c = [...arguments].forEach(element=>{
     let type = typeof element
     console.log(`${type}: ${element}`)
     if(!obj[type]) obj[type] = 0;
         obj[type]++  
 })
 Object.keys(obj).sort((a,b)=> obj[b] - obj[a]).forEach(key=> console.log(`${key} = ${obj[key]}`));
-
 }
+
 argumentInfo('cat', 42, function () { console.log('Hello world!');})
--- 04 Advanced Functions/Advanced Functions - Exercise/03fibonacci.js
+++ 04 Advanced Functions/Advanced Functions - Exercise/03fibonacci.js
@@ -1,9 +1,9 @@
-function getFibonator(){
-    let a =0 
-    let b = 1 
-    return ()=>{
-        let c = a+ b; 
-         a=b, b=c;  return a 
+function getFibonator() {
+    let a = 0
+    let b = 1
+    return () => {
+        let c = a + b;
+        a = b, b = c; return a
     }
 }
 let fib = getFibonator();

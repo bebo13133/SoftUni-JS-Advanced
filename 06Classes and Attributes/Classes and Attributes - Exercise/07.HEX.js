1eec1854d097cafb8c2f0a045c9d53db05f26a69
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-08 08:03:07 +0300

    Update 07.HEX.js

--- 06Classes and Attributes/Classes and Attributes - Exercise/07.HEX.js
+++ 06Classes and Attributes/Classes and Attributes - Exercise/07.HEX.js
@@ -25,7 +25,6 @@ class Hex {
     parse(data) {
         return parseInt(data, 16)
     }
-
 }
 let FF = new Hex(255); console.log(FF.toString()); FF.valueOf() + 1 == 256; let a = new Hex(10);
 let b = new Hex(5); console.log(a.plus(b).toString());

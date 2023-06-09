60b4e1c96216aa257e9fd16a8baa9467a63d89b1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-09 20:21:29 +0300

    Update app.js

--- Exams/01. Forum posts/app.js
+++ Exams/01. Forum posts/app.js
@@ -1,5 +1,19 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ...
+  let postInput = document.getElementById('post-title');
+  let categoryInput = document.getElementById('post-category');
+  let postContent = document.getElementById('post-content')
+
+  let publishBtn = document.getElementById('publish-btn');
+  publishBtn.addEventListener('click', onPublish)
+
+  function onPublish(e) {
+    const title = postInput.value;
+    const category = categoryInput.value;
+    const pContent = postContent.value;
+    if (title === "" || category === "" || pContent === "") { // samo probno
+      return;
+    }
+  }
 }

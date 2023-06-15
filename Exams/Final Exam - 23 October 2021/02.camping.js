fec58b383ea54cad094a31abadd8f82dcbba2b1d
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-15 18:58:35 +0300

    add

--- Exams/Final Exam - 23 October 2021/02.camping.js
+++ Exams/Final Exam - 23 October 2021/02.camping.js
@@ -10,8 +10,8 @@ class SummerCamp {
 
     registerParticipant(name, condition, money) {
 
-        if (!this.priceForTheCamp[condition])  throw new Error('Unsuccessful registration at the camp.');
-        
+        if (!this.priceForTheCamp[condition]) throw new Error('Unsuccessful registration at the camp.');
+
 
         const nameFind = this.listOfParticipants.find(x => x.name === name)
 
@@ -19,48 +19,48 @@ class SummerCamp {
         if (nameFind) return `The ${name} is already registered at the camp.`
 
         if (money < this.priceForTheCamp[condition]) return `The money is not enough to pay the stay at the camp.`
-    
-        this.listOfParticipants.push({name, condition, power: 100, wins:0})
+
+        this.listOfParticipants.push({ name, condition, power: 100, wins: 0 })
         return `The ${name} was successfully registered.`
     }
 
-    unregisterParticipant (name){
-        const findName = this.listOfParticipants.find(x=>x.name ===name)
-        if(!findName) throw new Error (`The ${name} is not registered in the camp.`)
-        const findIndex = this.listOfParticipants.findIndex(x=> x.name === name)
-        this.listOfParticipants.splice(findIndex,1)
+    unregisterParticipant(name) {
+        const findName = this.listOfParticipants.find(x => x.name === name)
+        if (!findName) throw new Error(`The ${name} is not registered in the camp.`)
+        const findIndex = this.listOfParticipants.findIndex(x => x.name === name)
+        this.listOfParticipants.splice(findIndex, 1)
         return `The ${name} removed successfully.`
     }
 
-    timeToPlay (typeOfGame, participant1, participant2){
-        let player1 = this.listOfParticipants.find(x=>x.name === participant1)
-        if(!player1) throw new Error (`Invalid entered name/s.`)
+    timeToPlay(typeOfGame, participant1, participant2) {
+        let player1 = this.listOfParticipants.find(x => x.name === participant1)
+        if (!player1) throw new Error(`Invalid entered name/s.`)
 
-        if(typeOfGame === 'Battleship'){
-            player1.power+=20
+        if (typeOfGame === 'Battleship') {
+            player1.power += 20
             return `The ${player1.name} successfully completed the game ${typeOfGame}.`
-        }else if(typeOfGame === 'WaterBalloonFights'){
+        } else if (typeOfGame === 'WaterBalloonFights') {
             let winner = ""
-            let player2 = this.listOfParticipants.find(x=> x.name === participant2)
+            let player2 = this.listOfParticipants.find(x => x.name === participant2)
             if (!player2) throw new Error(`Invalid entered name/s.`)
-            if(player1.condition !== player2.condition) throw new Error(`Choose players with equal condition.`)
-            if(player1.power>player2.power){
+            if (player1.condition !== player2.condition) throw new Error(`Choose players with equal condition.`)
+            if (player1.power > player2.power) {
                 player1.wins++
                 winner = player1.name
-            }else if(player2.power>player1.power){
+            } else if (player2.power > player1.power) {
                 player2.wins++
-                winner= player2.name
-            }else{
-                return  `There is no winner.`
+                winner = player2.name
+            } else {
+                return `There is no winner.`
             }
             return `The ${winner} is winner in the game ${typeOfGame}.`
         }
     }
-    toString () {
+    toString() {
 
         let printResult = `${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}`
-        let sorted = this.listOfParticipants.sort((a,b)=>b.wins - a.wins)
-        sorted.forEach(players=> printResult+=`\n${players.name} - ${players.condition} - ${players.power} - ${players.wins}`)
+        let sorted = this.listOfParticipants.sort((a, b) => b.wins - a.wins)
+        sorted.forEach(players => printResult += `\n${players.name} - ${players.condition} - ${players.power} - ${players.wins}`)
         return printResult
     }
 
---
+++ Exams/Retake Exam - 12 August 2021/02.artGallery.js
@@ -0,0 +1,92 @@
+class ArtGallery {
+    constructor(creator) {
+        this.creator = creator
+        this.possibleArticles = { "picture": 200, "photo": 50, "item": 250 }
+        this.listOfArticles = []
+        this.guests = []
+    }
+    addArticle(articleModel, articleName, quantity) {
+        if (!this.possibleArticles.hasOwnProperty(articleModel.toLowerCase())) throw new Error("This article model is not included in this gallery!");
+        const findName = this.listOfArticles.find(x => x.articleName === articleName && x.articleModel === articleModel.toLowerCase())
+        if (findName) {
+            findName.quantity += quantity
+        } else {
+            this.listOfArticles.push({ articleModel: articleModel.toLowerCase(), articleName, quantity })
+        }
+        return `Successfully added article ${articleName} with a new quantity- ${quantity}.`
+    }
+    inviteGuest(guestName, personality) {
+        let findGuest = this.guests.find(x => x.guestName === guestName);
+        if (findGuest) throw new Error(`${guestName} has already been invited.`)
+
+
+        if (!findGuest) {
+
+            if (personality === 'Vip') {
+                this.guests.push({ guestName, points: 500, purchaseArticle: 0 });
+
+            } else if (personality === 'Middle') {
+                this.guests.push({ guestName, points: 250, purchaseArticle: 0 });
+
+            } else {
+                this.guests.push({ guestName, points: 50, purchaseArticle: 0 });
+
+            }
+        }
+        return `You have successfully invited ${guestName}!`
+    }
+    buyArticle(articleModel, articleName, guestName) {
+        let finds = this.listOfArticles.find(x =>x.articleModel === articleModel.toLowerCase() && x.articleName === articleName)
+        
+        let findGuest = this.guests.find(x => x.guestName === guestName)
+
+        if (!finds) throw new Error("This article is not found.")
+
+        if (finds.quantity === 0) return `The ${articleName} is not available.`
+
+        if (!findGuest) return `This guest is not invited.`
+
+        if (findGuest.points < this.possibleArticles[articleModel]) {
+            return "You need to more points to purchase the article."
+        } else {
+            findGuest.points -= this.possibleArticles[articleModel]
+            finds.quantity--
+            findGuest.purchaseArticle++
+            return `${guestName} successfully purchased the article worth ${this.possibleArticles[articleModel]} points.`
+        }
+    }
+
+    showGalleryInfo (criteria){
+        let result = ''
+        if(criteria === 'article'){
+            result = `Articles information:`
+            this.listOfArticles.forEach(article => result+=`\n${article.articleModel} - ${article.articleName} - ${article.quantity}`)
+            return result
+        }
+         if(criteria === 'guest'){
+            result = `Guests information:`
+            this.guests.forEach(article=>result+=`\n${article.guestName} - ${article.purchaseArticle}`)
+            return result
+        }
+    
+    }
+}
+// const artGallery = new ArtGallery('Curtis Mayfield');
+// console.log(artGallery.addArticle('picture', 'Mona Liza', 3));
+// console.log(artGallery.addArticle('Item', 'Ancient vase', 2));
+// console.log(artGallery.addArticle('PICTURE', 'Mona Liza', 1));
+// const artGallery = new ArtGallery('Curtis Mayfield');
+// console.log(artGallery.inviteGuest('John', 'Vip'));
+// console.log(artGallery.inviteGuest('Peter', 'Middle'));
+// console.log(artGallery.inviteGuest('John', 'Middle'));
+const artGallery = new ArtGallery('Curtis Mayfield'); 
+artGallery.addArticle('picture', 'Mona Liza', 3);
+artGallery.addArticle('Item', 'Ancient vase', 2);
+artGallery.addArticle('picture', 'Mona Liza', 1);
+artGallery.inviteGuest('John', 'Vip');
+artGallery.inviteGuest('Peter', 'Middle');
+artGallery.buyArticle('picture', 'Mona Liza', 'John');
+artGallery.buyArticle('item', 'Ancient vase', 'Peter');
+console.log(artGallery.showGalleryInfo('article'));
+console.log(artGallery.showGalleryInfo('guest'));
+

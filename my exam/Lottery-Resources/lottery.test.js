67beaccc58b621b6b6d603e7f549eb8cfa661a06
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-18 10:20:38 +0300

    modify

--- my exam/Lottery-Resources/lottery.test.js
+++ my exam/Lottery-Resources/lottery.test.js
@@ -13,22 +13,6 @@ describe("Test functionality lottary",()=>{
             expect(()=>lottery.buyLotteryTicket('',-3,true)).to.throw("Invalid input!")
             expect(()=>lottery.buyLotteryTicket(3,"3",false)).to.throw("Unable to buy lottery ticket!")
             expect(()=>lottery.buyLotteryTicket('',3,false)).to.throw("Unable to buy lottery ticket!")
-            expect(()=>lottery.buyLotteryTicket(-1,3,false)).to.throw("Unable to buy lottery ticket!")
-            expect(()=>lottery.buyLotteryTicket([],"a",true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket("a",{},true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket(null,{},true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket(0,0,true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket('',0,true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket(0,'',true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket(1,0,true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket(0,1,true)).to.throw("Invalid input!")
-            expect(()=>lottery.buyLotteryTicket(1,1,false)).to.throw("Unable to buy lottery ticket!")
-            expect(()=>lottery.buyLotteryTicket(1,1,false)).to.throw("Unable to buy lottery ticket!")
-            expect(()=>lottery.buyLotteryTicket(1.1,1,false)).to.throw("Unable to buy lottery ticket!")
-            expect(()=>lottery.buyLotteryTicket(1,1.1,false)).to.throw("Unable to buy lottery ticket!")
-
-            
-
         })
         it("test valid input",()=>{
             expect(lottery.buyLotteryTicket(1,1,true)).to.equal("You bought 1 tickets for 1$.")
@@ -50,18 +34,9 @@ describe("Test functionality lottary",()=>{
             expect(()=> lottery.checkTicket(undefined,"")).to.throw("Invalid input!")
             expect(()=> lottery.checkTicket({},undefined)).to.throw("Invalid input!")
             expect(()=> lottery.checkTicket("","v")).to.throw("Invalid input!")
-            expect(()=> lottery.checkTicket(1,["v"])).to.throw("Invalid input!")
-            expect(()=> lottery.checkTicket(1.2,2)).to.throw("Invalid input!")
-            expect(()=> lottery.checkTicket(0,2)).to.throw("Invalid input!")
+
             expect(()=> lottery.checkTicket("",0)).to.throw("Invalid input!")
-            expect(()=>lottery.checkTicket([1,2,3,4,5],[6,7,8,9,10])).to.throw("Invalid input!")
-            expect(()=>lottery.checkTicket([1,2,3,4,5],[0,1,8,9,10])).to.throw("Invalid input!")
-            expect(()=>lottery.checkTicket([1,2,3],[6,7,8,9,10])).to.throw("Invalid input!")
-            expect(()=>lottery.checkTicket([1,2,3,4,5],[5,4,9,10])).to.throw("Invalid input!")
-            expect(()=>lottery.checkTicket([1,2,3,4,5],[1,2,10])).to.throw("Invalid input!")
-            //  expect(()=>lottery.checkTicket([1,2,3,4,5],[2,1,8,9,10])).to.throw("Invalid input!")   
-            //  expect(()=>lottery.checkTicket([1,2,3,4,5,6],[11,11,31,41,5,6])).to.throw("Invalid input!")   
-            //  expect(()=>lottery.checkTicket([1,2,3,4,5,6],[11,21,31,41,15,6])).to.throw("Invalid input!")   
+
               expect(()=>lottery.checkTicket([1,2,3,4,5,7.6],[11,21,31,41,15,6,7])).to.throw("Invalid input!")   
               expect(()=>lottery.checkTicket([1,2,3,4,5,6,7],[11,21,31,41,15,6])).to.throw("Invalid input!")   
 
@@ -77,7 +52,6 @@ describe("Test functionality lottary",()=>{
 
         })
     })
-
     describe("test secondChance method ",()=>{
         it('Test Invalid Inputs',()=>{
             expect(()=> lottery.secondChance("",[])).to.throw("Invalid input!")
@@ -89,12 +63,8 @@ describe("Test functionality lottary",()=>{
             expect(()=> lottery.secondChance("","")).to.throw("Invalid input!")
             expect(()=> lottery.secondChance(2,2)).to.throw("Invalid input!")
          
-          expect(()=> lottery.secondChance("-1",-1)).to.throw("Invalid input!")
-           
-            expect(() => lottery.secondChance("a", [1, 2, 3, 4, 5, 6,7,7.6])).to.throw("Invalid input!");
-            expect(() => lottery.secondChance("v", [1, 2, 3, 4, 5, 6,7,7.6])).to.throw("Invalid input!");
+            expect(()=> lottery.secondChance("-1",-1)).to.throw("Invalid input!") 
             expect(() => lottery.secondChance("a", [1, 2, 3, 4, 5, 6,])).to.throw("Invalid input!");
-
             expect(() => lottery.secondChance("a", [1, 2, 3, 4, 5, 6,7,7.6])).to.throw("Invalid input!");
             expect(() => lottery.secondChance(123.7, "b")).to.throw("Invalid input!");
 
@@ -104,8 +74,6 @@ expect(lottery.secondChance(244,[390, 244, 123])).to.equal("You win our second c
 expect(lottery.secondChance(100,[456, 789])).to.equal("Sorry, your ticket didn't win!")
 expect(lottery.secondChance(100,[100, 789, 100])).to.equal("You win our second chance prize!")
 expect(lottery.secondChance(120,[119, 244, 121])).to.equal("Sorry, your ticket didn't win!")
-
-
         })
     })
 })
